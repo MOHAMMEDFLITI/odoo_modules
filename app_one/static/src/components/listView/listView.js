c5b5b0ddb6c72a37c55c4dd83eda7f0d6fd4f4d0
Mohammed FLITI <mohammedfliti02@gmail.com>
2025-11-09 10:16:45 +0100

    OWL: Component Lifecycle.​ setup() & useState()

--- app_one/static/src/components/listView/listView.js
+++ app_one/static/src/components/listView/listView.js
@@ -1,12 +1,29 @@
 /* @odoo-module */
 
-import { Component } from "@odoo/owl";
-import { registry } from  "@web/core/registry";
+import { Component,useState } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 export class ListViewAction extends Component {
     // Component logic goes here
 
     static template = "app_one.ListView";
+    setup() { // OWL lifecycle method
+        // this.records = [ //  { id: 1, name: "Record One", description: "This is the first record.", postcode: "12345" },
+        // ]
+        this.state = useState({
+            records: [],
+        });
+        this.orm = useService("orm");
+        this.loadRecords();
+    }
+
+    async loadRecords() {
+        // Logic to load records from the backend or other sources
+        const result = await this.orm.searchRead("property", [], ["name", "descreption", "postcode"]);
+        console.log("Loaded records:", result);
+        this.state.records = result;
+    }
 }
 
 registry.category("actions").add("app_one.list_view_action", ListViewAction);
